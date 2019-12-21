dba030140a491ef54a929a64b359e62d35c60b49
Michał Gregorczyk <michalgr@fb.com>
2019-12-21 00:20:36 -0500

    add gnutls

---
+++ gnutls.mk
@@ -0,0 +1,8 @@
+GNUTLS_VERSION = 3.6.11
+
+gnutls: build/gnutls-$(GNUTLS_VERSION).autotools.sentinel
+
+build/gnutls-$(GNUTLS_VERSION).autotools: autogen nettle libtasn1 libunistring p11-kit unbound
+
+download/gnutls-$(GNUTLS_VERSION):
+	wget ftp://ftp.gnutls.org/gcrypt/gnutls/v3.6/gnutls-$(GNUTLS_VERSION).tar.xz -O $@
