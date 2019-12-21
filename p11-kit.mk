db94a7c1d60ac4eb0a1275e91d2eaedcb52e519b
Michał Gregorczyk <michalgr@fb.com>
2019-12-20 23:57:30 -0500

    add p11-kit

---
+++ p11-kit.mk
@@ -0,0 +1,8 @@
+P11_KIT_VERSION = 0.23.18.1
+
+p11-kit: build/p11-kit-$(P11_KIT_VERSION).autotools.sentinel
+build/p11-kit-$(P11_KIT_VERSION).autotools: libffi
+
+download/p11-kit-$(P11_KIT_VERSION):
+	mkdir -p download
+	wget https://github.com/p11-glue/p11-kit/releases/download/$(P11_KIT_VERSION)/p11-kit-$(P11_KIT_VERSION).tar.gz -O $@
