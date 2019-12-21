271453dab146e9839f1c40ee2fd341aa25bcad20
Michał Gregorczyk <michalgr@fb.com>
2019-12-21 00:25:15 -0500

    add emacs

---
+++ emacs.mk
@@ -0,0 +1,8 @@
+EMACS_VERSION = 26.3
+
+emacs: build/emacs-$(EMACS_VERSION).autotools.sentinel
+build/emacs-$(EMACS_VERSION).autotools: gnutls guile
+
+download/emacs-$(EMACS_VERSION):
+	mkdir -p download
+	wget https://ftp.gnu.org/gnu/emacs/emacs-$(EMACS_VERSION).tar.xz -O $@
