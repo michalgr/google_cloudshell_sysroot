625fd8a61b2d257dc6a7685b4483f3f2b79712af
Michał Gregorczyk <michalgr@fb.com>
2019-12-21 20:18:28 -0500

    autogen depends on libtool

--- autogen.mk
+++ autogen.mk
@@ -6,7 +6,7 @@ download/autogen-$(AUTOGEN_VERSION):
 	mkdir -p download
 	wget https://ftp.gnu.org/gnu/autogen/rel$(AUTOGEN_VERSION)/autogen-$(AUTOGEN_VERSION).tar.xz -O $@
 
-build/autogen-$(AUTOGEN_VERSION).autotools/.sentinel: sources/autogen-$(AUTOGEN_VERSION)/.sentinel guile
+build/autogen-$(AUTOGEN_VERSION).autotools/.sentinel: sources/autogen-$(AUTOGEN_VERSION)/.sentinel guile libtool
 	mkdir -p $(dir $@)
 	cd $(dir $@) && ../../sources/autogen-$(AUTOGEN_VERSION)/configure \
 		--prefix=$(SYSROOT) \
